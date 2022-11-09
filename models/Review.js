7fe30671f78d174ed3c3d2184e342c0038331d23
Will S <fitnesswill13@gmail.com>
2022-11-09 10:46:20 -0800

    models added

---
+++ models/Review.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model { }
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review',
+    }
+);
+
+module.exports = Review;
