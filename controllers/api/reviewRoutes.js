4c1d9f21b5eabdb31fe554e7ea45cb64483c9c91
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-11-09 12:49:11 -0800

    add reviewRoute

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { Review } = require("../../models");
+
+//POST route to create a review
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
