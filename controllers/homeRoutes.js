cd62ea68d3885e7ab118e65865396d482472e28b
Will S <fitnesswill13@gmail.com>
2022-11-10 10:12:51 -0800

    user profile added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { Product } = require('../models')
+const withAuth = require("../utils/auth");
+
 // GET route for display all products on the homepage
 router.get("/", async (req, res) => {
   const productsDataDb = await Product.findAll({});
@@ -12,6 +14,15 @@ router.get('/login', (req, res) => {
       return;
     }
     res.render('login');
+});
+router.get("/profile", withAuth, async (req, res) => {
+  const productsDataDb = await Product.findAll({
+    where: {
+      user_id: req.session.user_id
+    }
+  });
+  const products = productsDataDb.map((product) => product.get({plain:true}));
+  res.render("profile", {products, logged_in: req.session.logged_in });
 });
 
 module.exports = router;
