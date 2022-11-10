5d8b1276f8b8329f2e894d8845235e9f4962f7e4
Will S <fitnesswill13@gmail.com>
2022-11-10 10:23:51 -0800

    login btn, profile btn etc

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,7 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   const productsDataDb = await Product.findAll({});
   const products = productsDataDb.map((product) => product.get({plain:true}));
-  res.render("home", {products, logged_in: req.session.logged_in });
+  res.render("home", {products, logged_in: req.session.logged_in, homepage: true });
 });
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
