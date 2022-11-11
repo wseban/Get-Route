7db68b2bc2dadb4059b7239c1de164135fe13567
josephjensen <joe@cryptonumerics.com>
2022-11-10 17:46:43 -0800

    finalize buy button screen

---
+++ public/js/buyproduct.js
@@ -0,0 +1,19 @@
+const buyBtns = document.querySelectorAll(".buy-btn")
+buyBtns.forEach((button) => {
+    button.addEventListener('click', async (event) => {
+        const id = event.target.getAttribute('data-product-id')
+        console.log(id)
+        const response = await fetch(`/api/products/purchase/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+            await Swal.fire(
+                'Successfully Purchase!',
+                'Thanks for the patronage!',
+                'success'
+            )
+            document.location.reload()
+        }
+    })
+})
