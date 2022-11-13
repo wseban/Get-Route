e478c75d8029db075f471d15f6e83dcf9c576034
robogf <111620315+robogf@users.noreply.github.com>
2022-11-12 18:03:15 -0800

    tags are added to new products

--- public/js/addproduct.js
+++ public/js/addproduct.js
@@ -5,6 +5,12 @@ const createFormHandler = async (event) => {
   const description = document.querySelector("#description").value.trim();
   const price = document.querySelector("#price").value.trim();
   const stock = document.querySelector("#stock").value.trim();
+  var tag_ids = []
+  var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+  for (var i = 0; i < checkboxes.length; i++) {
+    tag_ids.push(checkboxes[i].getAttribute('data-tag-id'))
+  }
+
 
 
   if (isNaN(price)) {
@@ -14,7 +20,7 @@ const createFormHandler = async (event) => {
   if (name && description && price && stock) {
     const response = await fetch("/api/products/", {
       method: "POST",
-      body: JSON.stringify({ name, description, price, stock }),
+      body: JSON.stringify({ name, description, price, stock, tag_ids }),
       headers: { "Content-Type": "application/json" },
     });
 
